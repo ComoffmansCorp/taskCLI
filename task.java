15d6f838720d753bfa1c010bcdf1ce42484974b8
comoffmans <xreyser1337@gmail.com>
2025-07-03 03:54:52 +0400

    taskclass

---
+++ task.java
@@ -0,0 +1,19 @@
+public class task {
+    String nameTask;
+    int dateStart;
+    int dateEnd;
+    boolean completed;
+
+    public task(String name, int dateStart, int dateEnd, boolean completed) {
+        this.nameTask = name;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
+        this.completed = completed;
+    }
+
+    public task(){
+        
+    }
+
+    
+}
