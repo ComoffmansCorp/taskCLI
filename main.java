c167102e4aee7d516e57403810568a226ccf9388
comoffmans <xreyser1337@gmail.com>
2025-07-03 04:37:17 +0400

    main

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
 public class main {
-    
+    public static void main(String[] args) {
+        Task task = new Task(
+                "Написать код",
+                LocalDateTime.of(2025, 12, 31, 23, 25),
+                false
+        );
+        System.out.println(task);
+    }
 }
